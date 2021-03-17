5840b81ca422d5fd6c210a351bd0207a0fe04ed5
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-17 12:00:47 -0500

    set up basic create/show user actions with auth token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+    include AuthTokenConcern
+  
+    def create
+      user = User.create!(
+        username: params['username'],
+        password: params['password'],
+        password_confirmation: params['password'],
+        auth_token: unique_auth_token
+      )
+  
+      if user
+        session[:user_id] = user.id
+        render json: { user_id: user.id }
+      else
+        render json: { result: 'Unable to create new user' }
+      end
+    end
+
+
+    def show
+      user = User.find_by(username: params['username'])
+      
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: {user_id: user.id}
+      else
+        render json: {result: 'Unable to authenticate'}
+      end
+
+    end
 end
