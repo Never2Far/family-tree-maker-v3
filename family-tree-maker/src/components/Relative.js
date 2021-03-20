2d2c3db8831f26916f8cd235074714dbcf050491
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-20 16:36:50 -0500

    created Relative component to display a relative's info

---
+++ family-tree-maker/src/components/Relative.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import React, { Component } from 'react';
+// import ReviewsContainer from '../../containers/ReviewsContainer';
+
+// class Restaurant extends Component {
+  export const Relative = props => {
+    return (
+        <div>
+          {`${props.firsName} ${props.lastName} - ${props.relationship}`}
+          {/* <button onClick={this.handleOnClick}> X </button>
+          <ReviewsContainer restaurantId={restaurant.id}/> */}
+          </div>
+    );
+//   }
+};
+
+// export default Relative;
+
+
+
+//   handleOnClick = () => {
+//     this.props.deleteRestaurant(this.props.restaurant.id)
+//   }
+
+
+//   render() {
+//     const { restaurant } = this.props;
