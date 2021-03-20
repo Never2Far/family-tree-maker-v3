4642fcc76bd5dbe0287e5b89d73d10d444a23434
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-20 16:54:30 -0500

    reducer to manage relatives

---
+++ family-tree-maker/src/reducers/manageRelatives.js
@@ -0,0 +1,52 @@
+import uuid from 'uuid';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    relatives: manageRelatives,
+});
+
+export default rootReducer;
+
+function manageRelatives(state = [], action) {
+
+switch (action.type) {
+    case 'ADD_RELATIVE':
+        
+    const relative = {
+        id: uuid(),
+        firstName: action.payload.firstName,
+        lastName: action.payload.lastName,
+        relationship: action.payload.relationship
+    }
+        return [...state, relative]
+        // case 'DELETE_RESTAURANT':
+
+        // return state.filter(restaurant => restaurant.id !== action.id)
+
+    default:
+        return state
+}
+}
+
+// function manageReviews(state = [], action) {
+//     switch (action.type) {
+//         case 'ADD_REVIEW':
+            
+//         const review = {
+//             text: action.review.text,
+//             restaurantId: action.review.restaurantId,
+//             id: cuid()
+//         }
+//         if (review.restaurantId[0] === 'c') {
+//             return [...state, review]}
+//             else {
+//                 return state
+//             }
+//             case 'DELETE_REVIEW':
+    
+//             return state.filter(review => review.id !== action.id)
+          
+//         default:
+//             return state
+//         }
+//     }
