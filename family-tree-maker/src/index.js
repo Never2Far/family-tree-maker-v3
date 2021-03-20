0f8edfcd19c93d158bab538f7b043cc8f3d14018
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-20 17:00:36 -0500

    added redux and created store

--- family-tree-maker/src/index.js
+++ family-tree-maker/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import rootReducer from './reducers/manageRelatives'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
