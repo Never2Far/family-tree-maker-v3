ac5380eeabcaeefbc2233a90cd5ae9aba5afd129
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-21 17:22:52 -0500

    added uuid to users table for authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_195911) do
+ActiveRecord::Schema.define(version: 2021_03_21_203937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_195911) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "auth_token"
+    t.string "uuid"
   end
 
 end
