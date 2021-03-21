07f0d5e1ff852610a111e569afe90dd4f79f31d9
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-21 17:23:33 -0500

    added User params to actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,24 @@
 class UsersController < ApplicationController
-    # include AuthTokenConcern
+    include AuthTokenConcern
     # skip_before_action :verify_authenticity_token
   
     def create
         # puts params.to_s
+
       user = User.create!(
+        uuid: params['id'],
+        first_name: params['firstName'],
+        last_name: params['lastName'],
         username: params['username'],
         password: params['password'],
         password_confirmation: params['password'],
-        # auth_token: unique_auth_token
+        auth_token: unique_auth_token
       )
   
       if user
         # session[:user_id] = user.id
-        render json: { user_id: user.id,
+        render json: { uuid: user.uuid,
+          userToken: user.auth_token,
                         success: true }
       else
         render json: { error: 'Unable to create new user',
@@ -26,10 +31,12 @@ class UsersController < ApplicationController
       user = User.find_by(username: params['username'])
       
       if user.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: {user_id: user.id}
+        # session[:user_id] = user.id
+        render json: {userToken: user.auth_token,
+        success: true}
       else
-        render json: {result: 'Unable to authenticate'}
+        render json: {error: 'Unable to authenticate',
+        success: false}
       end
 
     end
