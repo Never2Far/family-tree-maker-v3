e680d1d3ff73e18526ad0e34cca7529c481ff97c
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-21 17:16:39 -0500

    added thunk

--- family-tree-maker/src/index.js
+++ family-tree-maker/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import rootReducer from './reducers/manageRelatives'
+import rootReducer from './reducers/rootReducer'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+// import reportWebVitals from './reportWebVitals';
+
+// const store = createStore(rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+  const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  composeEnhancer(applyMiddleware(thunk))
+  );
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,4 +27,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
