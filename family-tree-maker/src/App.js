110f4037b1d0ca5c0a3b1d1c1acfce15a7d2c743
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-22 08:57:31 -0500

    Added React Router and initial Nav Links

--- family-tree-maker/src/App.js
+++ family-tree-maker/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import AuthContainer from './containers/AuthContainer';
 // import './App.css';
 import RelativesContainer from './containers/RelativesContainer';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router,
+Switch,
+Route,
+Link } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <RelativesContainer />
-      <AuthContainer />
-    </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route path='/relatives' render={() => <RelativesContainer />}/>
+          <Route path='/signup' render={() => <AuthContainer />}/>
+          <Route path='/login' render={() => <div>Login</div>} />
+        </div>
+      </Router>
   );
 }
 
