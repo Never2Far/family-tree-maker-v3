4896bff336271b07eeeb0b4cbb6431b54037abc6
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-24 11:02:43 -0500

    logic to resolve user id from auth0 to user in db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,40 @@
 class UsersController < ApplicationController
+  include CUID
     include AuthTokenConcern
     # skip_before_action :verify_authenticity_token
   
     def create
-        # puts params.to_s
+        puts params
+        user = User.find_by(email: params['email'])
 
-      user = User.create!(
-        uuid: params['id'],
-        first_name: params['firstName'],
-        last_name: params['lastName'],
-        username: params['username'],
-        password: params['password'],
-        password_confirmation: params['password'],
-        auth_token: unique_auth_token
-      )
+        if !user
+          
+          user = User.create(
+            uuid: CUID::generate(),
+            email: params['email'],
+            first_name: params['given_name'],
+            last_name: params['family_name'],
+            # username: params['username'],
+            # password: params['password'],
+            # password_confirmation: params['password'],
+            auth_token: unique_auth_token
+          )
+        end
+
+        render json: {uuid: user.uuid,
+                      userToken: user.auth_token,
+                      success: true
+                      }
   
-      if user
-        # session[:user_id] = user.id
-        render json: { uuid: user.uuid,
-          userToken: user.auth_token,
-                        success: true }
-      else
-        render json: { error: 'Unable to create new user',
-        success: false }
-      end
+      # if user
+      #   # session[:user_id] = user.id
+      #   render json: { uuid: user.uuid,
+      #     userToken: user.auth_token,
+      #                   success: true }
+      # else
+      #   render json: { error: 'Unable to create new user',
+      #   success: false }
+      # end
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  post 'signup' => 'users#create'
+  post 'resolve' => 'users#create'
+  # post 'signup' => 'users#create'
   post 'login' => 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
