b827d99a66eb2755ae66ed2f4fd4526ef61dcca6
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-24 10:40:09 -0500

    conveted to class component and attatched deleteRelative prop

--- family-tree-maker/src/components/Relative.js
+++ family-tree-maker/src/components/Relative.js
@@ -1,21 +1,34 @@
-import React from 'react';
-// import React, { Component } from 'react';
-// import ReviewsContainer from '../../containers/ReviewsContainer';
+import React, { Component } from 'react';
 
-// class Restaurant extends Component {
- const Relative = props => {
+class Relative extends Component {
+//  const Relative = props => {
+
+
+handleOnClick = () => {
+     const payload = {relativeId: this.props.relativeId}
+      this.props.deleteRelative(payload)
+}
+
+
+
+render() {
     return (
         
-          <div>
-          {`${props.relative.firstName} ${props.relative.lastName} - ${props.relative.relationship}`}
-          </div>
+          <li>
+          {`${this.props.relative.firstName} ${this.props.relative.lastName} - ${this.props.relative.relationship}`}
+          <button onClick={this.handleOnClick}> X </button>
+          </li>
           
-    );
-//   }
+    )}
+
 };
 
 export default Relative;
 
+// import React, { Component } from 'react';
+// import ReviewsContainer from '../../containers/ReviewsContainer';
+
+// class Restaurant extends Component {
 
 
 //   handleOnClick = () => {
@@ -25,3 +38,17 @@ export default Relative;
 
 //   render() {
 //     const { restaurant } = this.props;
+
+//     return (
+//       <div>
+//         <li>
+//           {restaurant.text}
+//           <button onClick={this.handleOnClick}> X </button>
+//           <ReviewsContainer restaurantId={restaurant.id}/>
+//           </li>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Restaurant;
