b60ab6c5c4cdef95f05aea30aab9be906fe668cd
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-24 13:43:58 -0500

    generated Relative resource

--- family-tree-maker-backend/config/routes.rb
+++ family-tree-maker-backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :relatives
   resources :users
   post 'resolve' => 'users#create'
   # post 'signup' => 'users#create'
---
+++ family-tree-maker-backend/db/migrate/20210324184133_create_relatives.rb
@@ -0,0 +1,13 @@
+class CreateRelatives < ActiveRecord::Migration[6.0]
+  def change
+    create_table :relatives do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :relationship
+      t.text :relativeId
+      t.text :userId
+
+      t.timestamps
+    end
+  end
+end
