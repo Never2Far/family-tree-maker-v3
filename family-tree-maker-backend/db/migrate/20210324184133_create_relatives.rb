81a3516cfae637fd4fc193ed5ff987eb21a96ff4
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-25 20:54:13 -0500

    chnaged name format to match frontend's keys

--- family-tree-maker-backend/db/migrate/20210324184133_create_relatives.rb
+++ family-tree-maker-backend/db/migrate/20210324184133_create_relatives.rb
@@ -1,8 +1,12 @@
 class CreateRelatives < ActiveRecord::Migration[6.0]
   def change
     create_table :relatives do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :firstName
+      t.string :middleName
+      t.string :lastName
+      t.string :nickname
+      t.string :altName
+      t.text :notes
       t.string :relationship
       t.text :relativeId
       t.text :userId
