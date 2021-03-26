4807284ebc021fd6ea9646fa9f1c38c4043c1337
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-25 20:53:49 -0500

    removed login-related fields

--- family-tree-maker-backend/db/migrate/20210316211929_create_users.rb
+++ family-tree-maker-backend/db/migrate/20210316211929_create_users.rb
@@ -3,11 +3,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :username
-      t.string :password_digest
       t.string :email
       t.string :full_name
-
+      t.text :userId
       t.timestamps
     end
   end
