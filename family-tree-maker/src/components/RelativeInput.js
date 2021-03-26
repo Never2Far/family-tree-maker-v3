b9815713f86446c8c420e89c9068981acb0626b7
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-26 15:57:56 -0500

    Added Bootstrap form components

--- family-tree-maker/src/components/RelativeInput.js
+++ family-tree-maker/src/components/RelativeInput.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+// import FormGroup from 'react-bootstrap/esm/FormGroup';
+// import FormLabel from 'react-bootstrap/esm/FormLabel';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
 
 class RelativeInput extends Component {
 
@@ -31,7 +37,67 @@ class RelativeInput extends Component {
 
   render() {
     return (
-      <div>
+      
+      
+        <Container>
+          <h3>Add New Relative</h3>
+        <Form inline onSubmit={(event) => this.handleOnSubmit(event)}>
+        
+        <Form.Group controlId="formGroupFirstName">
+          <Form.Label>First Name:</Form.Label>
+          <Form.Control
+          size="sm"
+           type='text'
+           name='firstName'
+           onChange={(event) => this.handleOnChange(event)}
+           value={this.state.firstName}/>
+        </Form.Group>
+
+        <Form.Group controlId="formGroupLastName">
+        <Form.Label>Last Name:</Form.Label>
+        <Form.Control 
+        size="sm"
+          type='text'
+          name='lastName'
+          onChange={(event) => this.handleOnChange(event)}
+          value={this.state.lastName}
+        />
+        </Form.Group>
+
+        <Form.Group controlId="formGroupRelationship">
+        <Form.Label>Relationship:</Form.Label>
+        <Form.Control 
+        as="select"
+        size="sm"
+          
+          name='relationship'
+          onChange={(event) => this.handleOnChange(event)}
+          value={this.state.relationship}
+        >
+        <option hidden selected value>Select one...</option>
+        <option>Spouse</option>
+        <option>Father</option>
+        <option>Mother</option>
+        <option>Sibling</option>
+        <option>Child</option>
+        </Form.Control>
+        </Form.Group>
+
+        <Button variant="primary" type='submit'>
+          Submit
+        </Button>
+        
+     
+      </Form>
+      </Container>
+    );
+  }
+};
+
+export default RelativeInput;
+
+
+/* <div>
         <h2>Add New Relative</h2>
         <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <label>First Name:</label>
@@ -57,9 +123,4 @@ class RelativeInput extends Component {
         />
         <input type='submit'/>
         </form>
-      </div>
-    );
-  }
-};
-
-export default RelativeInput;
+      </div> */
