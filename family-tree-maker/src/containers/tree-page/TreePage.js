5a3bf6461137347b5969b3ff530ab5d3905d7b77
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-04-01 13:55:25 -0500

    added grid container to tree page to wrap tree

--- family-tree-maker/src/containers/tree-page/TreePage.js
+++ family-tree-maker/src/containers/tree-page/TreePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 import Container from 'react-bootstrap/Container';
+import GridContainer from './GridContainer'
 
 const TreePage = () => {
 
+    const relatives = useSelector(state => state.relatives.relatives)
 
     return (
         <Container>
             <h1>Family Tree</h1>
+            <GridContainer relatives={relatives}/>
         </Container>
     )
 }
