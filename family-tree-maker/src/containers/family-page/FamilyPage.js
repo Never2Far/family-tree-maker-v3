11f56b8d0983e13e8cb8f46ed51e126178783751
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-04-01 11:16:08 -0500

    added Relative Input form component

--- family-tree-maker/src/containers/family-page/FamilyPage.js
+++ family-tree-maker/src/containers/family-page/FamilyPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import RelativeInput from '../../components/RelativeInput'
 
 const FamilyPage = () => {
 
     return (
         <Container>
             <h1>My Family</h1>
+            <RelativeInput />
         </Container>
     )
 }
