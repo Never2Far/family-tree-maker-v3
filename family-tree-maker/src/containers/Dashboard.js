d3f91354811262006cbbad8537cc2953af8eef19
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-31 21:29:32 -0500

    added Dashboard container to wrap home page

---
+++ family-tree-maker/src/containers/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+const Dashboard = () => {
+
+    return (
+        <Container>
+            <Jumbotron>
+                <h1>Dashboard</h1>
+            </Jumbotron>
+        </Container>
+    )
+
+}
+
+export default Dashboard
