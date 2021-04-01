8a45c6c9ee20a9fab2627d6749d1becf8bc9d4dd
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-31 21:29:08 -0500

    added TreePage container to wrap Family Tree

---
+++ family-tree-maker/src/containers/TreePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+const TreePage = () => {
+
+
+    return (
+        <Container>
+            <h1>Family Tree</h1>
+        </Container>
+    )
+}
+
+export default TreePage
