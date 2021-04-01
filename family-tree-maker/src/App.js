8d3145b616b576b6b5fa3bfc9b1b883fa1b048ec
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-31 21:12:59 -0500

    moved routes up to App component

--- family-tree-maker/src/App.js
+++ family-tree-maker/src/App.js
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-// import { useSelector, useDispatch } from 'react-redux';
-import './App.css';
-import RelativesContainer from './containers/RelativesContainer';
-import NavBar from './components/NavBar';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,30 +9,42 @@ import {
   useLocation,
   useParams
 } from "react-router-dom";
-import history from "./router/history";
+import NavBar from './components/NavBar';
+import ProfilePage from './containers/ProfilePage'
+
+import RelativesContainer from './containers/RelativesContainer';
+
+
+// import history from "./router/history";
 // import { useAuth0 } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { fetchRelatives } from './actions/fetchRelatives';
 import RelativeShow from './components/RelativeShow'
 import MainSwitch from './router/MainSwitch';
 // import Switch from 'react-bootstrap/esm/Switch';
 
 
-function App(props) {
+const App = () => {
 
- const dispatch = useDispatch()
-
-useEffect(() => {
-  dispatch(fetchRelatives(props.userId))
-}, [])
+  const {user, logout} = useAuth0();
+  const userId = user.sub
 
   return (
    
-       <Router history={history}>
-        <div>
+       <Router >
           <NavBar />
-          <MainSwitch />
-        </div>
+          <Switch>
+            <Route exact path="/" render={() => <h2>Welcome, {user.name}!</h2>} />
+
+            <Route path='/profile'>
+              <ProfilePage />
+            </Route>
+
+            <Route path='/relatives/:relativeId' children={<RelativeShow />} />
+
+            <Route path='/relatives' children={<RelativesContainer />}/>
+
+            <Route path='/logout' render={() => <button onClick={logout({ returnTo: window.location.origin })}>Log Out</button>} />
+          </Switch>
        </Router> 
       
   );}
