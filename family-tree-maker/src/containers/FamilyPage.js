668817c4c1b739a9c29ed7a8cd3eeaff32625e1b
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-31 21:30:04 -0500

    added FamilyPage container to wrap relative list and input

---
+++ family-tree-maker/src/containers/FamilyPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+const FamilyPage = () => {
+
+    return (
+        <Container>
+            <h1>My Family</h1>
+        </Container>
+    )
+}
+
+export default FamilyPage
