411015897af741c4ec5cb2cdb8814584944d0913
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-03-31 21:47:24 -0500

    added iniitial user details

--- family-tree-maker/src/containers/profile-page/ProfilePage.js
+++ family-tree-maker/src/containers/profile-page/ProfilePage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import {useAuth0} from '@auth0/auth0-react'
 
 const ProfilePage = () => {
+    const {user} = useAuth0();
 
     return (
         <Container>
+            
             <h1>About Me</h1>
+            <Jumbotron>
+            <span>Name: {user.name}</span>
+            <br></br>
+            <span>Email: {user.email}</span>
+            </Jumbotron>
         </Container>
     )
 
