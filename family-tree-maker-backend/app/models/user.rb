edc1c396bd0fcf1e9f6799375f5e9e1541ff6a18
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-04-10 22:10:36 -0500

    added attr_accessors--may not be necessary, will test

--- family-tree-maker-backend/app/models/user.rb
+++ family-tree-maker-backend/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :relatives
     # has_secure_password
     # validates :username, uniqueness: {case_sensitive: false}
+    attr_accessor :email, :lastName, :firstName, :fullName, :userId
 
 end
