4f1ff446cd9e23e27e7ba1da7f775b27f004b1e1
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-04-21 11:52:46 -0500

    fixed dashboard navlink

--- family-tree-maker/src/components/NavBar.js
+++ family-tree-maker/src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
       <NavLink 
         style={{ marginRight: '10px' }} 
-        to="/"
+        to="/dashboard"
       >
         Dashboard
       </NavLink>
