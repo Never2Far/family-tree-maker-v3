9e2d24fa55cc79c6415d47cb8db36e2b23c3e53f
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-04-21 10:58:53 -0500

    get user info from state instead of auth0

--- family-tree-maker/src/containers/profile-page/ProfilePage.js
+++ family-tree-maker/src/containers/profile-page/ProfilePage.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import {useAuth0} from '@auth0/auth0-react'
 
 const ProfilePage = () => {
     const {user} = useAuth0();
+    const userInfo = useSelector(state => state.users.userInfo)
 
     return (
         <Container>
             
             <h1>About Me</h1>
             <Jumbotron>
-            <span>Name: {user.name}</span>
+            <span>Name: {userInfo.firstName}</span>
             <br></br>
-            <span>Email: {user.email}</span>
+            <span>Email: {userInfo.email}</span>
             </Jumbotron>
         </Container>
     )
