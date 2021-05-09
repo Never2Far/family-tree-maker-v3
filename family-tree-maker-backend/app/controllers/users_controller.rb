9637741c1f68f8be141567c563a27d46f12794b9
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-05-09 16:30:17 -0500

    changed put method to patch for update user details from server

--- family-tree-maker-backend/app/controllers/users_controller.rb
+++ family-tree-maker-backend/app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   # include CUID
     # include AuthTokenConcern
     # skip_before_action :verify_authenticity_token
-  
     def find_or_create
       puts request.headers
         request.headers.inspect
@@ -27,7 +26,7 @@ class UsersController < ApplicationController
           userInfo: user,
         success: true
         }
-      elsif !user['firstName'] || !user['lastName'] || user['firstName'] == "" || user['lastName'] == ""
+      elsif user && (!user['firstName'] || !user['lastName'] || user['firstName'] == "" || user['lastName'] == "")
     render json: {
       needUserInfo: true,
       userInfo: user,
@@ -40,9 +39,10 @@ class UsersController < ApplicationController
     render json: {
       needUserInfo: false,
       # userInfo: user,
-      userInfo: {userId: user['userId'],
-                  firstName: user['firstName'],
-                lastName: user['lastName']},
+      # userInfo: {userId: user['userId'],
+      #             firstName: user['firstName'],
+      #           lastName: user['lastName']},
+      userInfo: user,
     success: true
     }
   else
@@ -72,29 +72,74 @@ class UsersController < ApplicationController
     def update
       puts user_params
 
+      user_fields = ['email', 'lastName', 'firstName', 'fullName', 'city', 'state', 'zip', 'birthdate',
+        'birthplace', 'phone', 'address', 'middleName', 'nickname', 'altName']
+
       user = User.find_by(userId: user_params[:userId])
 
       if !user
         # user = User.create(user_params)
-        user = User.new()
-        user['userId']= user_params[:userId]
-        user['firstName']= user_params[:firstName]
-          user['lastName']= user_params[:lastName]
-          user['email']= user_params[:email]
+        user = User.new(user_params)
+        # user['userId']= user_params[:userId]
+        # user['firstName']= user_params[:firstName]
+        #   user['lastName']= user_params[:lastName]
+        #   user['email']= user_params[:email]
           puts user.inspect
           user.save
+
+          # user_info = user.inspect.select { |k,v|
+          #   user_fields.include?(k)
+          # }
+
+          user_info = {}
+
+          user_fields.each do |field|
+            if user[field] == nil
+              user_info[field] = ""
+            else
+            user_info[field] = user[field]
+            end
+          end
           
-      elsif !user['firstName'] || !user['lastName'] || user['firstName'] == "" || user['lastName'] == ""
-          user['firstName']= user_params[:firstName]
-          user['lastName']= user_params[:lastName]
-        # user = user.update(user_params)
-        user.save
-      end
+          # user_fields.map{|field|
+          #   if user[field] == null
+          #     return  
+          #  user[field]
+          # }
+
+          render json: {
+            userInfo: user_info,
+          success: true
+          }
+          
+      # elsif user && (!user['firstName'] || !user['lastName'] || user['firstName'] == "" || user['lastName'] == "")
+      #     user['firstName']= user_params[:firstName]
+      #     user['lastName']= user_params[:lastName]
+      #   # user = user.update(user_params)
+      #   user.save
       
-      if user
+      
+    elsif user
+
+        user.update(user_params)
+
+
+
+        user_info = {}
+
+        user_fields.each do |field|
+          if user[field] == nil
+            user_info[field] = ""
+          else
+          user_info[field] = user[field]
+          end
+        end
+
+
         puts user.inspect
+        puts user_info.inspect
         render json: {
-          userInfo: user,
+          userInfo: user_info,
         success: true
         }
       else
--- family-tree-maker-backend/config/routes.rb
+++ family-tree-maker-backend/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'relatives' => 'relatives#create'
   put 'relatives' => 'relatives#update'
   get 'users' => 'users#find_or_create'
-  put 'users' => 'users#update'
+  patch 'users' => 'users#update'
   # post 'signup' => 'users#create'
   post 'login' => 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
