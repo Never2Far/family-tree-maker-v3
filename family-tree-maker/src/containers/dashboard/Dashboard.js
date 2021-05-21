e30544d26979114d9f64e4af5d47d62bcb7f1b5d
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-05-21 10:59:57 -0500

    Added usage instructions

--- family-tree-maker/src/containers/dashboard/Dashboard.js
+++ family-tree-maker/src/containers/dashboard/Dashboard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import {Link} from 'react-router-dom'
 
 const Dashboard = () => {
 
     return (
         <Container>
             <Jumbotron>
-                <h1>Dashboard</h1>
+                <h2>Welcome to the Family Tree Maker!</h2>
+                <ol>
+                    <li>Edit your information by clicking on <Link to={`/profile`}>Profile</Link>.</li>
+                    <li>Add your relatives using the <Link to={`/relatives`}>My Family</Link> page.</li>
+                    <li>Edit your relative's details by clicking on their name.</li>
+                    <li>View your tree by clicking <Link to={`/tree`}>Tree</Link></li>
+                </ol>
             </Jumbotron>
         </Container>
     )
