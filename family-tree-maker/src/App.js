cc60953dc75da96331dfea52b040608e885c5b00
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-08-12 16:05:31 -0500

    added basename prop to Router component for GH pages routing

--- family-tree-maker/src/App.js
+++ family-tree-maker/src/App.js
@@ -1,77 +1,69 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import NavBar from './components/NavBar';
-import Dashboard from './containers/dashboard/Dashboard'
-import ProfilePage from './containers/profile-page/ProfilePage'
-import FamilyPage from './containers/family-page/FamilyPage'
-import TreePage from './containers/tree-page/TreePage'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import RelativeShowContainer from './containers/RelativeShowContainer'
+import NavBar from "./components/NavBar";
+import Dashboard from "./containers/dashboard/Dashboard";
+import ProfilePage from "./containers/profile-page/ProfilePage";
+import FamilyPage from "./containers/family-page/FamilyPage";
+import TreePage from "./containers/tree-page/TreePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RelativeShowContainer from "./containers/RelativeShowContainer";
 
 const App = () => {
+  const needUserInfo = useSelector((state) => state.users.needUserInfo);
 
-  const needUserInfo = useSelector(state => state.users.needUserInfo)
+  const { logout } = useAuth0();
 
-  const {logout} = useAuth0();
- 
-console.log(needUserInfo)
+  console.log(needUserInfo);
   return (
-   
-       <Router >
-          <NavBar />
-          <Switch>
-            <Route exact path="/">
-             
-              <Redirect to='/home'/>
-            </Route>
+    <Router basename={process.env.PUBLIC_URL}>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
 
+        <Route path="/home">
+          <Dashboard />
+        </Route>
 
-            <Route path='/home'>
-              <Dashboard />
-              
-            </Route>
+        <Route path="/profile/edit">
+          <ProfilePage editing={true} />
+        </Route>
 
-            <Route path='/profile/edit'>
-              <ProfilePage editing={true}/>
-            </Route>
+        <Route path="/profile">
+          <ProfilePage editing={false} />
+        </Route>
 
-            <Route path='/profile'>
-              <ProfilePage editing={false}/>
-            </Route>
+        <Route path="/relatives/:relativeId/edit">
+          <RelativeShowContainer editing={true} />
+        </Route>
 
-            
+        <Route path="/relatives/:relativeId">
+          <RelativeShowContainer editing={false} />
+        </Route>
 
-            <Route path='/relatives/:relativeId/edit'>
-              <RelativeShowContainer editing={true}/>
-            </Route>
+        <Route path="/relatives">
+          <FamilyPage />
+        </Route>
 
-            <Route path='/relatives/:relativeId'>
-              <RelativeShowContainer editing={false}/>
-            </Route>
+        <Route path="/tree">
+          <TreePage />
+        </Route>
 
-        
-
-            <Route path='/relatives'>
-              <FamilyPage />
-            </Route>
-
-            <Route path='/tree'>
-              <TreePage />
-            </Route>
-
-            <Route path='/logout' render={() => <button onClick={logout()}>Log Out</button>} />
-          </Switch>
-       </Router> 
-      
-  );}
- 
+        <Route
+          path="/logout"
+          render={() => <button onClick={logout()}>Log Out</button>}
+        />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
-
