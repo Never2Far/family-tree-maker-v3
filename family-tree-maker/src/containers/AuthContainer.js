b084ff8a4dc39b57e1f43b6ccf40f55aaecefa31
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-08-15 16:28:40 -0500

    added loading spinner / removed console.logs

--- family-tree-maker/src/containers/AuthContainer.js
+++ family-tree-maker/src/containers/AuthContainer.js
@@ -1,45 +1,32 @@
-import React from 'react';
-import { useDispatch } from 'react-redux'
-import App from '../App'
-import { useAuth0 } from '@auth0/auth0-react';
-
-import { fetchUser } from '../actions/fetchUser';
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import App from "../App";
+import { useAuth0 } from "@auth0/auth0-react";
+import { fetchUser } from "../actions/fetchUser";
+import Spinner from "react-bootstrap/Spinner";
 
 const AuthContainer = () => {
   const dispatch = useDispatch();
-    const {
-        isLoading,
-        isAuthenticated,
-        error,
-        user,
-        loginWithRedirect
-      } = useAuth0();
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect } =
+    useAuth0();
 
-
- if (isLoading) {
-    return <div>Loading...</div>;
+  if (isLoading) {
+    return (
+      <div>
+        <Spinner />
+        Loading...
+      </div>
+    );
   }
   if (error) {
     return <div>Oops... {error.message}</div>;
   }
   if (isAuthenticated) {
-
-console.log(user)
-console.log(user.given_name)
-
-dispatch(fetchUser(user))
-
-
-  
-return (
-<App />
-)
+    dispatch(fetchUser(user));
+    return <App />;
+  } else {
+    loginWithRedirect();
   }
-  else {
-    loginWithRedirect()
-  }
+};
 
-}
-
-export default AuthContainer
+export default AuthContainer;
